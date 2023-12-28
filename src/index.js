1970a94ec15383791328d1e07c851b1a8a791f87
syed jaffry <sajaffry@gmail.com>
2023-12-27 20:47:54 -0800

    removed redundant aws amplify reference

--- src/index.js
+++ src/index.js
@@ -4,13 +4,9 @@ import 'semantic-ui-css/semantic.min.css'
 import './index.css';
 import App from './App';
 import TextInput from './TextInput';
-import { Amplify } from 'aws-amplify';
-import config from './aws-exports';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 document.title = 'Feedback app';
-
-Amplify.configure(config);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
